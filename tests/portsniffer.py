36e3a5cf7c80bbace92cd1b3df047419fcf326a8
boldm <bm456@ya.ru>
2022-12-27 16:35:44 +0400

    Fixed sniffer halting

--- tests/portsniffer.py
+++ tests/portsniffer.py
@@ -1,19 +1,16 @@
 from serial import Serial
+from time import sleep
 
-hostport = Serial('COM3', timeout = 0)
-printerport = Serial('COM7', timeout =0)
+hostport = Serial('COM3', timeout=0)
+printerport = Serial('COM7', timeout=0)
 
 logfile = open('conection.log', 'w', encoding='UTF-8')
-# logfile.write('hello')
-# logfile.close()
 while (True):
     hline = hostport.readline()
-    # print(hline)
     if hline:
         print([hline])
         logfile.write('Host: '+str(hline)+'\n')
         logfile.flush()
-        # logfile.write('Hello\n')
         printerport.write(hline)
 
     pline = printerport.readline()
@@ -22,3 +19,4 @@ while (True):
         logfile.write('Printer: '+str(pline)+'\n')
         logfile.flush()
         hostport.write(pline)
+    sleep(0.01)
