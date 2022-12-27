543477827e95fcb688e7c3358e6fb1051ee16281
boldm <bm456@ya.ru>
2022-12-27 22:07:34 +0400

    added statup to gsender

--- tests/gsender.py
+++ tests/gsender.py
@@ -32,8 +32,21 @@ def reciever():
         if line:
             print(line.decode('ascii'))
 
-
-txthread = Thread(target=sender)
-txthread.start()
-rxthred = Thread(target=reciever)
-rxthred.start()
+if __name__=='__main__':
+    port.write(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\n')    
+    port.write(b'N1 M110*34\r\n')
+    port.write(b'N2 M115*36\r\n')
+    port.write(b'N3 M105*36\r\n')
+    port.write(b'N4 M114*35\r\n')
+    port.write(b'N5 M111 S6*98\r\n')
+    port.write(b'N6 T0*60\r\n')
+    port.write(b'N7 M20*22\r\n')
+    port.write(b'N8 M80*19\r\n')
+    port.write(b'N9 M105*46\r\n')
+    port.write(b'N10 M111 S6*86\r\n')
+    port.write(b'N11 T0*10\r\n')
+    port.write(b'N12 M155 S1*83\r\n')
+    txthread = Thread(target=sender)
+    txthread.start()
+    rxthred = Thread(target=reciever)
+    rxthred.start()
