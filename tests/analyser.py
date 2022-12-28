dd18063e92d51b0804cfabd14d1b05017b3a2612
boldm <bm456@ya.ru>
2022-12-28 21:30:19 +0400

    added data catch

---
+++ tests/analyser.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+file = open('tests/recieve.log', 'r')
+x = []
+y = []
+t = []
+i = 0
+for line in file.readlines():
+    data = line.strip().split('\t')
+    try:
+        if not i:
+            x0=float(data[0])
+            y0 = float(data[1])
+        x.append((float(data[0])-x0)/100)
+        y.append((float(data[1])-y0)/10000/256)
+        t.append(i/1000)
+        i+=1
+    except:
+        print(f'Failed to read data from line {i}: {data}')
+
+fig, ax = plt.subplots()
+
+ax.plot(t,y)
+# ax.plot(t,x)
+ax.set(xlim=(t[0], t[-1]))
+plt.show()
